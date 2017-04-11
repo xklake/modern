4654c665ae3114f1757b022daab3180bf6cb4628
Qiang Li <joneleesz@hotmail.com>
2017-04-11 18:36:16 +0100

    去掉header文件

--- layouts/header.php
+++ layouts/header.php
@@ -20,29 +20,7 @@
                 </div>
 
                 <div class="col-sm-6 col-xs-8">
-                    <!--div class="social">
-                        <ul class="social-share">
-                            <li><a href="<?=Yii::$app->setting->get('facebook')?>"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="<?=Yii::$app->setting->get('twitter')?>"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="<?=Yii::$app->setting->get('linkedin')?>"><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="<?=Yii::$app->setting->get('dribbble')?>"><i class="fa fa-weixin"></i></a></li>
-                            <li><a href="<?=Yii::$app->setting->get('skype')?>"><i class="fa fa-skype"></i></a></li>
-                        </ul>
 
-                        <div class="search">
-                            <?php if (Yii::$app->user->isGuest) { ?>
-                                <i class="fa fa-user"></i><a class="uk-icon-user"  href="<?= Yii::$app->urlManager->createAbsoluteUrl(['blog/default/signin']) ?>" rel="nofollow">请登录</a><s>|</s><a href="<?= Yii::$app->urlManager->createAbsoluteUrl(['blog/default/signup']) ?>" rel="nofollow">免费注册</a>
-                            <?php } else { ?>
-                                <i class="fa fa-user"></i><a href="<?= Yii::$app->urlManager->createAbsoluteUrl(['/blog/default/usercentre']) ?>">
-                                    <?= isset(Yii::$app->user->identity->profile->surname) ? Yii::$app->user->identity->profile->surname : Yii::$app->user->identity->username ?></a>&nbsp;[<a href="<?= Yii::$app->urlManager->createUrl(['blog/default/logout']) ?>">退出</a>]
-                            <?php } ?>
-
-                            <form action="<?= Yii::$app->urlManager->createUrl(['/blog/default/catalog']) ?>" method="get" id="search_fmr" name="search_fmr" role="form">
-                                <input class="search-form" autocomplete="off" type="text" name="keyword" id="searchText" placeholder="搜索博客"/>
-                                <i class="fa fa-search"></i>
-                            </form>
-                        </div>
-                    </div-->
                 </div>
             </div>
         </div><!--/.container-->
